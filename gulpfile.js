6299cfa73993a0590a2c2e3b061906564f8155e9
Pavel Kozhevov <kozhevov@mail.ru>
2017-05-26 11:18:55 +0700

    Remove folder "css"

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,11 @@
+'use strict';
+
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    bs = require('browser-sync'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglifyjs');
+    sass = require('gulp-sass');
 
-
-var libsJs = [
-    'app/libs/jquery/dist/jquery.min.js'
-];
-
-
-gulp.task('scripts', function () {
-    return gulp.src(libsJs)
-        .pipe(concat('libs.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('app/js'));
-});
 
 gulp.task('sass', function () {
-    return gulp.src('app/sass/**/*.sass')
-        .pipe(sass())
-        .pipe(gulp.dest('app/css'))
-        .pipe(bs.reload({stream: true}))
-});
-
-gulp.task('browser-sync', function () {
-    bs({
-        server: {
-            baseDir: 'app'
-        },
-        notify: false
-    });
-});
-
-gulp.task('watch', ['browser-sync', 'sass'], function () {
-    gulp.watch('app/sass/**/*.sass', ['sass']);
-    gulp.watch('app/*.html', bs.reload);
+   return gulp.src('app/sass/**/*.sass')
+       .pipe(sass())
+       .pipe(gulp.dest('app/css'))
 });
